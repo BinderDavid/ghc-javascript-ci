6f575f25dfdaeb73c26007120c1f2141c2d9ea97
David Binder <david.binder@uni-tuebingen.de>
2024-02-13 15:24:23 +0100

    Put cabal configuration in cabal.project

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import Foreign.C.String
+
+foreign import javascript "((arr,offset) => document.body.innerHTML = h$decodeUtf8z(arr,offset))"
+  setInnerHtml :: CString -> IO ()
+
+circle :: String
+circle = "<svg width=300 height=300><circle cx=50% cy=50% r=50%></circle></svg>"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = withCString circle setInnerHtml
